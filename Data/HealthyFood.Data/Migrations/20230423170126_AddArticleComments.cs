52f012d3b3f20b69b8964fbc2ae4c41ecbe373fc
Stanislav Stoyanov <slavkata_99@abv.bg>
2023-04-23 20:39:07 +0300

    Add migration for ArticleComments model

---
+++ Data/HealthyFood.Data/Migrations/20230423170126_AddArticleComments.cs
@@ -0,0 +1,80 @@
+﻿#nullable disable
+
+namespace HealthyFood.Data.Migrations
+{
+    using System;
+
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    /// <inheritdoc />
+    public partial class AddArticleComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ArticleComments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ArticleId = table.Column<int>(type: "int", nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleComments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ArticleComments_ArticleComments_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "ArticleComments",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ArticleComments_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ArticleComments_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleComments_ArticleId",
+                table: "ArticleComments",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleComments_IsDeleted",
+                table: "ArticleComments",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleComments_ParentId",
+                table: "ArticleComments",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleComments_UserId",
+                table: "ArticleComments",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleComments");
+        }
+    }
+}
